a6643899422057fd3d9ca0274feb946a9e1c0004
junsung - infonet <kimjsung200012@gmail.com>
2025-03-26 18:44:30 +0900

    simplified rename function on main.py edir .gitignore

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import cv2
 
 from tqdm import tqdm
 
+# will remove this function after the dataset is fixed
 def rename_dir():
-    dir_name = {
+    exp_dir = {
+        "exp_rotated": "04",
         "exp0_rotated": "02",
         "exp1_rotated": "07",
         "exp2_rotated": "06",
@@ -15,28 +17,56 @@ def rename_dir():
         "exp7_rotated": "01",
         "exp8_rotated": "10",
         "exp9_rotated": "11",
-        "exp10_rotated": "",
-        "exp11_rotated": "",
-        "exp12_rotated": "",
-        "exp13_rotated": "",
-        "exp14_rotated": "",
-        "exp15_rotated": "",
-        "exp16_rotated": "",
+        "exp10_rotated": "17",
+        "exp11_rotated": "15",
+        "exp12_rotated": "12",
+        "exp13_rotated": "18",
+        "exp14_rotated": "13",
+        "exp15_rotated": "16",
+        "exp16_rotated": "14",
+        "exp17_rotated": "20",
+        "exp18_rotated": "19",
+        "exp19_rotated": "21",
+        "exp20_rotated": "22",
     }
 
-    os.chdir("dataset")
-    lst = [i for i in os.listdir() if (("exp" in i) and ("(need edit)" not in i))]
+    rgb_dir = {
+        "WIN_20250320_16_52_53_Pro": "07",
+        "WIN_20250320_17_23_35_Pro": "06",
+        "WIN_20250320_18_06_51_Pro": "05",
+        "WIN_20250321_13_46_47_Pro": "09",
+        "WIN_20250321_13_59_08_Pro": "08",
+        "WIN_20250321_14_14_08_Pro": "03",
+        "WIN_20250321_14_45_22_Pro": "01",
+        "WIN_20250321_14_59_43_Pro": "10",
+        "WIN_20250321_15_14_34_Pro": "11",
+        "WIN_20250323_11_12_21_Pro": "17",
+        "WIN_20250323_11_21_33_Pro": "15",
+        "WIN_20250323_11_32_18_Pro": "12",
+        "WIN_20250323_11_43_49_Pro": "18",
+        "WIN_20250323_11_53_04_Pro": "13",
+        "WIN_20250323_12_03_59_Pro": "16",
+        "WIN_20250323_12_28_30_Pro": "14",
+        "WIN_20250324_14_58_45_Pro": "20",
+        "WIN_20250324_15_25_20_Pro": "19",
+        "WIN_20250325_14_32_12_Pro": "21",
+        "WIN_20250326_16_07_56_Pro": "22",
+    }
+
+    lst = [i for i in os.listdir("dataset") if (("exp" in i) and (f"{exp_dir[i]}HSI" not in os.listdir(f"dataset/{i}")[0])) or (("WIN" in i) and (f"RGB{rgb_dir[i]}" not in os.listdir("dataset/{i}")[0]))]
 
     for i in lst:
-        elems = os.listdir(i)
+        if ("exp" in i):
+            elems = [i for i in os.listdir("dataset/" + i) if ".bmp" in i]
 
-        for hsi in elems:
-            if dir_name[i] + "HSI" in hsi:
-                continue
+            for hsi in elems:
+                if exp_dir[i] + "HSI" in hsi:
+                    continue
 
-            os.rename(f"{i}/{hsi}", f"{i}/{dir_name[i]}HSI{hsi}")
+                os.rename(f"dataset/{i}/{hsi}", f"dataset/{i}/{exp_dir[i]}HSI{hsi}")
 
-    os.chdir("..")
+        elif ("WIN" in i):
+            os.rename(f"dataset/{i}", f"dataset/RGB{rgb_dir[i]}")
 
 def old_video_to_image():
     lst = [i for i in os.listdir("raw-dataset")]
@@ -90,7 +120,7 @@ def new_hsi_rotate():
             cX, cY = w // 2, h // 2
             M = cv2.getRotationMatrix2D((cX, cY), 180, 1.0)
             image = cv2.warpAffine(image, M, (w, h))
-            cv2.imread(f"{target_path}/{elem}", image)
+            cv2.imwrite(f"{target_path}/{elem}", image)
 
 def new_video_to_image():
     lst = [i for i in os.listdir("raw-dataset") if "WIN" in i and i[:-4] not in os.listdir("dataset")]
@@ -115,10 +145,11 @@ def new_video_to_image():
         video.release()
 
 def main():
-    #rename_dir()
+    #new_hsi_rotate()
+    #new_video_to_image()
+    rename_dir()
     #old_video_to_image()
-    new_video_to_image()
-    new_hsi_rotate()
+    
 
 if __name__ == "__main__":
     main()
